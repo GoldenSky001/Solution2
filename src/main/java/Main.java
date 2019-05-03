12220501c1d689479b7a4aff2df2cb22bba57776
GoldenSky001 <245878155@qq.com>
2019-05-03 11:07:25 +0800

    增加了一行输出

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("a new update");
     }
 }
