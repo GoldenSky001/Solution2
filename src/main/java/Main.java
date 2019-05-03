bc0220d2545cc608fd108c548f8191d8376916c6
GoldenSky001 <245878155@qq.com>
2019-05-03 11:09:03 +0800

    增加

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("a new update");
+        System.out.println("增加第二行输出");
     }
 }
